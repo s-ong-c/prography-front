ff71793bb01f04db079367620eb035f6b4fc5d48
s-ong-c <somony9292@gmail.com>
2019-08-29 22:32:43 +0900

    basic 구조

---
+++ todo/src/containers/MainContainers.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const MainContainersBlock = styled.div``;
+interface MainContainersProps{}
+
+const MainContainers: React.SFC<MainContainersProps> = props => {
+  return <MainContainersBlock />;
+  };
+
+export default MainContainers;
